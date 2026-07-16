8b1fa2c7f5a30c84e83177680fc2d2754121f034
David Stauffer <david.c.stauffer@lmco.com>
2026-07-16 11:43:30 -0700

    Better main() testing.

--- nubs/tests/test_cli.py
+++ nubs/tests/test_cli.py
@@ -10,20 +10,120 @@ Notes
 
 # %% Imports
 import contextlib
+import inspect
 import io
+import os
+import pathlib
 import unittest
+from unittest.mock import patch
 
 import nubs as nubs
+
+
+# %% get_root_dir
+class Test_get_root_dir(unittest.TestCase):
+    r"""
+    Tests the get_root_dir function with the following cases:
+        call the function
+    """
+
+    def test_function(self) -> None:
+        filepath = inspect.getfile(nubs.get_root_dir.__wrapped__)
+        expected_root = pathlib.Path(os.path.split(filepath)[0])
+        folder = nubs.get_root_dir()
+        self.assertEqual(folder, expected_root)
+        self.assertTrue(folder.is_dir())
 
 
 # %% main
 class Test_main(unittest.TestCase):
     r"""
     Tests the main function with the following cases:
-        TBD
+        Help (good and bad)
+        Version (good and bad)
+        Doctests (pass and fail)
+        Unit Tests (pass and fail)
     """
 
-    pass  # TODO: write this  # noqa: PIE790
+    def test_help(self) -> None:
+        buffer = io.StringIO()
+        with contextlib.redirect_stdout(buffer), patch("sys.argv", ["name.py", "help"]), self.assertRaises(SystemExit) as exc:
+            nubs.main()
+        output = buffer.getvalue()
+        buffer.close()
+        self.assertEqual(exc.exception.code, 0)
+        self.assertTrue(output.startswith("####\nnubs\n####\n"))
+
+    def test_bad_help(self) -> None:
+        with (
+            patch("nubs.cli.print_help", return_value=nubs.cli._ReturnCodes.bad_help_file),  # noqa: SLF001
+            patch("sys.argv", ["name.py", "--help"]),
+            self.assertRaises(SystemExit) as exc,
+        ):
+            nubs.main()
+        self.assertEqual(exc.exception.code, 3)
+
+    def test_version(self) -> None:
+        buffer = io.StringIO()
+        with (
+            contextlib.redirect_stdout(buffer),
+            patch("sys.argv", ["name.py", "version"]),
+            self.assertRaises(SystemExit) as exc,
+        ):
+            nubs.main()
+        output = buffer.getvalue()
+        buffer.close()
+        self.assertEqual(exc.exception.code, 0)
+        self.assertIn(".", output)
+
+    def test_bad_version(self) -> None:
+        with (
+            patch("nubs.cli.print_version", return_value=nubs.cli._ReturnCodes.bad_version),  # noqa: SLF001
+            patch("sys.argv", ["name.py", "--version"]),
+            self.assertRaises(SystemExit) as exc,
+        ):
+            nubs.main()
+        self.assertEqual(exc.exception.code, 4)
+
+    def test_doctests(self) -> None:
+        with (
+            patch("nubs.cli.doctest.testfile", return_value=(0, "")) as mock_tester,
+            patch("sys.argv", ["name.py", "doctests", "-v"]),
+            self.assertRaises(SystemExit) as exc,
+        ):
+            nubs.main()
+        self.assertEqual(exc.exception.code, 0)
+        mock_tester.assert_any_call(str(nubs.get_root_dir().joinpath("cli.py")), report=True, verbose=True, module_relative=False)  # fmt: skip
+
+    def test_doctest_fails(self) -> None:
+        with (
+            patch("nubs.cli.doctest.testfile", return_value=(1, "")) as mock_tester,
+            patch("sys.argv", ["name.py", "doctests"]),
+            self.assertRaises(SystemExit) as exc,
+        ):
+            nubs.main()
+        self.assertEqual(exc.exception.code, 5)
+        mock_tester.assert_any_call(str(nubs.get_root_dir().joinpath("cli.py")), report=True, verbose=False, module_relative=False)  # fmt: skip
+
+    def test_unittests(self) -> None:
+        with (
+            patch("pytest.main", return_value=0) as mock_tester,
+            patch("sys.argv", ["name.py", "tests"]),
+            self.assertRaises(SystemExit) as exc,
+        ):
+            nubs.main()
+        self.assertEqual(exc.exception.code, 0)
+        mock_tester.assert_called_with([str(nubs.get_root_dir().joinpath("tests")), "-rfEsP"])
+
+    def test_bad_unittests(self) -> None:
+        with (
+            patch("pytest.main", return_value=-1) as mock_tester,
+            patch("sys.argv", ["name.py", "tests", "--extra"]),
+            self.assertRaises(SystemExit) as exc,
+        ):
+            nubs.main()
+        self.assertEqual(exc.exception.code, 5)
+        mock_tester.assert_called_with([str(nubs.get_root_dir().joinpath("tests")), "-rfEsP", "--extra"])
 
 
 # %% print_help
